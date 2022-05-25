2749410d726969383cf24b5fe29aa6f2a5b83c90
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-05-25 15:17:40 +0530

    change in terrafrom output

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -119,6 +119,6 @@ output "target" {
 }
 
 output "instance_endpoint" {
-  value       = join("", aws_db_instance.employee.*.endpoint)
+  value       = join("", aws_db_instance.employee.*.address)
   description = "DNS Endpoint of the instance"
 }
